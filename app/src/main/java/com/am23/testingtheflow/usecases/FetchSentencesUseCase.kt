31a9a46a4d2596b02e1834aff589dbd8591789cc
Florent Blot <florent@geev.com>
2023-04-20 23:56:41 +0200

    injection: change dispatchers in usecases

--- app/src/main/java/com/am23/testingtheflow/usecases/FetchSentencesUseCase.kt
+++ app/src/main/java/com/am23/testingtheflow/usecases/FetchSentencesUseCase.kt
@@ -1,15 +1,17 @@
 package com.am23.testingtheflow.usecases
 
 import com.am23.testingtheflow.services.SentencesService
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 
 class FetchSentencesUseCase(
-    private val sentencesService: SentencesService = SentencesService()
+    private val sentencesService: SentencesService = SentencesService(),
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
 
     operator fun invoke(): Flow<String> =
         sentencesService.fetchSentences()
-            .flowOn(context = Dispatchers.IO)
+            .flowOn(context = dispatcher)
 }
--- app/src/main/java/com/am23/testingtheflow/usecases/FetchSentencesWithDelayUseCase.kt
+++ app/src/main/java/com/am23/testingtheflow/usecases/FetchSentencesWithDelayUseCase.kt
@@ -1,15 +1,17 @@
 package com.am23.testingtheflow.usecases
 
 import com.am23.testingtheflow.services.SentencesService
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 
 class FetchSentencesWithDelayUseCase(
-    private val sentencesService: SentencesService = SentencesService()
+    private val sentencesService: SentencesService = SentencesService(),
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
 
     operator fun invoke(): Flow<String> =
         sentencesService.fetchSentencesWithDelay()
-            .flowOn(context = Dispatchers.IO)
+            .flowOn(context = dispatcher)
 }
